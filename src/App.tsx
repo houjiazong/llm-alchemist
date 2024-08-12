92a1aba7cbe43e930d14f24f9afd0340138a6e44
houjiazong <houjiazong@gmail.com>
2024-08-12 21:07:04 +0800

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center">
+        <img src="/logo.svg" width={200} />
+        <div className="text-xl">LLM Alchemist</div>
+      </div>
+    </>
+  )
+}
+
+export default App
