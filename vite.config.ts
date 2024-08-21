7b664bdefaf0a93adf4d909abcaa3097bb1ed01c
houjiazong <houjiazong@gmail.com>
2024-08-21 14:56:57 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
-import devProxyConfig from './dev.proxy.config'
+import fs, { constants } from 'node:fs/promises'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(async () => {
+  let devProxyConfig = {}
+
+  try {
+    await fs.access('./dev.proxy.config.ts', constants.F_OK)
+    devProxyConfig = (await import('./dev.proxy.config')).default
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    console.log('No dev.proxy.config.ts found, skipping proxy setup.')
+  }
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    proxy: devProxyConfig,
-  },
+    server: {
+      proxy: devProxyConfig,
+    },
+  }
 })
